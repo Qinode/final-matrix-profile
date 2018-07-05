e5b2acab2ad94fd2cd7bf363fb84136cba43d46d
Zheng Qin <zq17@matrix14.doc.ic.ac.uk>
2018-07-05 16:24:02 +0100

    reshape data in pattern_mds method

--- src/mds/util.py
+++ src/mds/util.py
@@ -1,4 +1,6 @@
 import numpy as np
+from src.mds.cmds import cmdscale
+import scipy.spatial.distance
 
 
 def standarization(data):
@@ -19,3 +21,15 @@ def standarization(data):
             data[i] = data[i] - mean
 
     return data
+
+
+def pattern_mds(idx, window_size, time_series):
+    data = np.zeros((len(idx), window_size))
+
+    for i, value in enumerate(idx):
+        data[i] = time_series[value: value+window_size].reshape(data[i].shape)
+
+    z_data = standarization(data)
+    pd = scipy.spatial.distance.pdist(z_data)
+    pdm = scipy.spatial.distance.squareform(pd)
+    return cmdscale(pdm)
