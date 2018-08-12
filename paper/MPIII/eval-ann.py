7b7874ceff38e5d415849e531569aef735b62aad
Zheng Qin <zq17@point25.doc.ic.ac.uk>
2018-08-12 18:36:33 +0100

    ann added

---
+++ paper/MPIII/eval-ann.py
@@ -0,0 +1,179 @@
+import numpy as np
+import time
+import datetime
+import scipy.io
+import os
+
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import as_completed
+from src.matrix_profile.matrixprofile import stomp
+
+
+from paper.MPIII.visual import subsequence_selection, sax_subsequence_selection, approximate_subsequence_selction
+from paper.MPIII.util import *
+
+
+def get_timestampe():
+    return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_f(idxs, tp, p, window_size):
+    precision = []
+    recall = []
+    hit = 0
+
+    for i in range(idxs.shape[0]):
+        if np.min(np.abs(tp - idxs[i])) < p * window_size:
+            hit += 1
+
+        precision.append(hit / (i + 1))
+        recall.append(hit / tp.shape[0])
+
+    return np.array(precision), np.array(recall)
+
+
+def f1(precisions, recalls):
+
+    if len(precisions) == 0 or len(recalls) == 0:
+        return 0
+
+    precision = precisions[-1]
+    recall = recalls[-1]
+
+    return np.around(2.0 / ((1.0/precision) + (1.0/recall)), decimals=3)
+
+
+def run(dataset, data_name, save, save_path, bounded=False):
+    mat_file = scipy.io.loadmat(dataset)
+    data = mat_file['data']
+    mp = mat_file['matrixProfile']
+    mpi = mat_file['profileIndex'] - 1
+
+    # matrix profile will be computed in scaled and z-normalised space
+    # rather than original space
+    # z_data = (data - np.mean(data))/(np.std(data))
+    # uz_data = (z_data - np.min(z_data))/(np.max(z_data) - np.min(z_data))
+
+    tp = mat_file['labIdx'] - 1
+
+    p = 0.2
+
+    window_size = int(mat_file['subLen'][0][0])
+
+    t_min, t_max = discretization_pre(data, window_size)
+
+    x_axis = np.arange(3, 12)
+    valid_idx_arr = []
+
+    for bits in x_axis:
+        print('[{}] {} - {} bits'.format(get_timestampe(), data_name, bits))
+        # 30/07/2018
+        # used for testing the the behaviour of digitising the time series before the matrix profile is computed.
+        # the result is in eval_fig/z_norm_mp
+
+        # q_data = np.digitize(uz_data, interval)
+        # print('[{}] {} Computing Matrix Profile.'.format(get_timestampe(), data_name))
+        # mp, mpi = stomp(q_data, q_data, window_size, True)
+        # print('[{}] {} Computing Matrix Profile Finished.'.format(get_timestampe(), data_name))
+
+        # if save:
+        #     scipy.io.savemat('{}/{}-mp'.format(save_path, data_name), {'mp': mp, 'mpi': mpi})
+
+        start = time.time()
+        c, h, compress_table, idx_bitsave = approximate_subsequence_selction(data, t_min, t_max, mp, mpi, window_size, bits)
+        end = time.time()
+        times = end - start
+        print('[{}] {} - {}bits DNorm Compression time {}.'.format(get_timestampe(), data_name, bits, times))
+
+        idx_bitsave = np.array(idx_bitsave)
+
+        cut_off = np.argmin(idx_bitsave[:, 1])
+
+        valid_idx = idx_bitsave[:, 0][:cut_off]
+
+        valid_idx_arr.append(valid_idx.shape[0])
+
+        precisions, recalls = get_f(valid_idx, tp, p, window_size)
+
+        if save:
+            scipy.io.savemat('{}/saved-data/dnorm-{}bits'.format(save_path, bits),
+                             {'compressible': c, 'hypothesis': h, 'compress_table': str(compress_table),
+                              'idx_bitsave': idx_bitsave, 'precisions': precisions, 'recalls': recalls,
+                              'process_time': times})
+
+
+        plt.plot(idx_bitsave[:, 1], label='DNorm', color='C0')
+        plt.axvline(cut_off, linewidth=1, label='DNorm Cut Off', color='C0')
+        plt.legend()
+        plt.title('{} bits compression'.format(bits))
+        plt.ylabel('Bits')
+        plt.xlabel('Number of Components')
+        if save:
+            plt.savefig('{}/bits/{} bits.png'.format(save_path, bits))
+        else:
+            plt.show()
+        plt.clf()
+
+        plt.plot(recalls, precisions, label='DNorm', color='C0')
+        plt.legend()
+
+        plt.title('{} bits compression\n DNorm {}'.format(bits, f1(precisions, recalls)))
+        plt.ylabel('Precision')
+        plt.xlabel('Recall')
+        if save:
+            plt.savefig('{}/recall-precision/{} bits.png'.format(save_path, bits))
+        else:
+            plt.show()
+        plt.clf()
+
+    plt.plot(x_axis, valid_idx_arr, label='DNorm')
+    plt.legend()
+    plt.title('Components vs Compression Bit')
+    if save:
+        plt.savefig('{}/components/components-bits.png'.format(save_path))
+    plt.clf()
+    # plt.show()
+
+    return '[{}] {} finished.'.format(get_timestampe(), data_name)
+
+
+# if __name__ == '__main__':
+#     import matplotlib.pyplot as plt
+#
+#     run('./eval_data/Plane', 'Plane', False, '', bounded=False)
+
+if __name__ == '__main__':
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    eval_path = os.path.join(path, 'eval_data')
+    data = os.listdir(eval_path)
+
+    sub_dirs = ['recall-precision', 'bits', 'components', 'saved-data']
+
+    pool = ProcessPoolExecutor(7)
+    futures =[]
+
+    parmas = []
+
+    for d in data:
+        data_name = d[:-4]
+
+        data_path = os.path.join(eval_path, data_name)
+        fig_path = os.path.join(path, 'eval_fig/ann_selection', data_name)
+        # fig_path = os.path.join(path, 'eval_fig/z_norm_mp', data_name)
+
+        for dir_name in sub_dirs:
+            os.makedirs(os.path.join(fig_path, dir_name))
+
+        parmas.append((data_path, data_name, fig_path))
+
+    for p in parmas:
+        futures.append(pool.submit(run, p[0], p[1], True, p[2], bounded=False))
+
+    for f in as_completed(futures):
+        print(f.result())
+
+    pool.shutdown()
--- paper/MPIII/visual.py
+++ paper/MPIII/visual.py
@@ -4,6 +4,89 @@ from paper.MPIII.util import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+def approximate_subsequence_selction(ts, t_min, t_max, mp, mpi, window_size, bits, trees=100):
+    from annoy import AnnoyIndex
+
+    mp, mpi = mp.copy(), mpi.copy()
+    max_patterns = ts.shape[0] // window_size
+
+    candidates, candidate_idx = pick_candidates(ts, t_min, t_max, window_size, mp, bits, max_patterns)
+
+    ann_indexer = AnnoyIndex(window_size, metric='euclidean')
+
+    for i in range(len(candidate_idx)):
+        item = ts[i:i+window_size]
+        item = (item - np.mean(item))/np.std(item)
+        ann_indexer.add_item(i, item)
+        # ann_indexer.add_item(i, candidates[i])
+
+    ann_indexer.build(trees)
+
+    C, C_idx, H, H_idx = [], [], [], []
+    idx_bitsave = []
+    compress_table = {}
+
+    U = ts.shape[0] - window_size + 1
+    bit_cost = U * window_size * bits
+
+    for i in range(len(candidate_idx)):
+        if len(H) == 0:
+            H.append(candidates[i])
+            H_idx.append(candidate_idx[i])
+            continue
+
+        mpi_nn = mpi[candidate_idx[i]][0]
+        quantized_mpi_nn = discretization(ts[mpi_nn:mpi_nn+window_size], t_min, t_max, bits)
+        hypo_save = window_size * bits - rdl(quantized_mpi_nn, candidates[i], bits)
+
+        approximate_nn = ann_indexer.get_nns_by_item(i, 2)
+        if len(approximate_nn) == 0:
+            print('Approximate nearest neighbour not found')
+            H.append(candidates[i])
+            H_idx.append(candidate_idx[i])
+            continue
+        else:
+            ann = approximate_nn[1]
+            compress_save = window_size * bits - rdl(candidates[i], candidates[ann], bits)
+
+        if hypo_save > compress_save:
+            H.append(candidates[i])
+            H_idx.append(candidate_idx[i])
+
+            if len(idx_bitsave) == 0:
+                idx_bitsave.append([candidate_idx[i], bit_cost, 0])
+            else:
+                previous_cost = idx_bitsave[-1][1]
+                idx_bitsave.append([candidate_idx[i], previous_cost, 0])
+
+            U -= 1
+
+            if candidate_idx[i] not in compress_table:
+                compress_table[candidate_idx[i]] = []
+
+        else:
+            C.append(candidates[i])
+            C_idx.append(candidate_idx[i])
+
+            if(candidate_idx[ann] not in H_idx):
+                H.append(candidates[ann])
+                H_idx.append(candidate_idx[ann])
+                U -= 1
+                # print('Hypothesis not included yet.')
+
+            U -= 1
+            new_cost = bit(C, H, U, window_size, bits)
+            bit_cost = min(bit_cost, new_cost)
+
+            idx_bitsave.append([candidate_idx[i], new_cost, 1])
+
+            if candidate_idx[ann] not in compress_table:
+                compress_table[candidate_idx[ann]] = [candidate_idx[i]]
+            else:
+                compress_table[candidate_idx[ann]].append(candidate_idx[i])
+
+    return C_idx, H_idx, compress_table, idx_bitsave
+
 def subsequence_selection(time_series, t_min, t_max, mp, mpi, window_size, nums, bits):
     max_salient = np.around(time_series.shape[0] / window_size)
     mp, mpi = mp.copy(), mpi.copy()
@@ -132,8 +215,9 @@ if __name__ == "__main__":
     t_min, t_max = discretization_pre(ts, window_size)
 
     for b in [4]:
-        interval = sax_discretization_pre(ts, b, bounded=False)
-        c, h, compress_table, idx_bitsave = sax_subsequence_selection(ts, interval, t_min, t_max, mp, mpi, window_size, 10, b)
+        # interval = sax_discretization_pre(ts, b, 'norm', bounded=False)
+        # c, h, compress_table, idx_bitsave = sax_subsequence_selection(ts, interval, t_min, t_max, mp, mpi, window_size, 10, b)
+        c, h, compress_table, idx_bitsave = approximate_subsequence_selction(ts, t_min, t_max, mp, mpi, window_size, b)
         idx_bitsave = np.array(idx_bitsave)
 
         cut_off = np.where(np.diff(idx_bitsave[:, 1]) > 0)[0]
@@ -143,11 +227,13 @@ if __name__ == "__main__":
         else:
             cut_off = cut_off[0]
 
+        cut_off = np.argmin(idx_bitsave[:, 1])
+
         valid_idx = idx_bitsave[:, 0][:cut_off]
         precisions, recalls = get_f(valid_idx, tp, 0.2, window_size)
 
         plt.plot(recalls, precisions)
-        plt.title('{} bits compression\n Gaussian {}'.format(b, f1(precisions, recalls)))
+        plt.title('{} bits compression\n Dnorm {}'.format(b, f1(precisions, recalls)))
         plt.ylabel('Precision')
         plt.xlabel('Recall')
         plt.show()
